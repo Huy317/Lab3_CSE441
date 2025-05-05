870f7b1b914f1604d4e27b82611a0811fdde6cf0
Huy317 <167590855+Huy317@users.noreply.github.com>
2025-05-05 16:13:40 +0700

    added bottom nav

--- ProductsApp/App.tsx
+++ ProductsApp/App.tsx
@@ -4,14 +4,35 @@ import {
   Text,
   View,
 } from 'react-native';
-import { Button } from 'react-native-paper';
+import { BottomNavigation, Button } from 'react-native-paper';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import ProductList from './ProductList';
 
 
 function App(): React.JSX.Element {
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    {key: 'ProductList', title: 'Products', icon: 'home'},
+    {key: 'ProductAdd', title: 'Add', icon: 'plus'},
+    {key: 'ProductSearch', title: 'Search', icon: 'magnify'},
+    {key: 'ProductDetail', title: 'Detail', icon: 'format-list-bulleted'},
+  ]);
+  const renderScene = BottomNavigation.SceneMap({
+    ProductList: ProductList,
+    ProductAdd: ProductList,
+    ProductSearch: ProductList,
+    ProductDetail: ProductList,
+  })
   return(
-      <ProductList />
+      <SafeAreaProvider>
+        <BottomNavigation
+          navigationState={{ index, routes }}
+          onIndexChange={setIndex}
+          renderScene={renderScene}
+        />
+      </SafeAreaProvider>
+
+    
   );
 }
 
