0406083baad7f78cb6a6a4585501931c43bfdf2a
Huy317 <167590855+Huy317@users.noreply.github.com>
2025-05-05 15:59:46 +0700

    moved ProductList to a separate file

--- ProductsApp/App.tsx
+++ ProductsApp/App.tsx
@@ -6,7 +6,7 @@ import {
 } from 'react-native';
 import { Button } from 'react-native-paper';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-
+import ProductList from './ProductList';
 type itemProps = {
   imgUrl: string,
   title: string,
@@ -20,83 +20,11 @@ type itemProps = {
 
 
 function App(): React.JSX.Element {
-  const [data, setData] = useState([]);
-  const filePath = 'https://dummyjson.com/products';
-  useEffect(() => {
-    fetch(filePath)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((d) => {
-        setData(d.products);
-      })
-      .catch((error) => {
-        console.error('Error fetching data: ', error);
-      });
-  });
-  const Item = ({ imgUrl, title, description, price, rating, brand,discountPercentage }: itemProps) => (
-    // <View style={{ marginVertical: 10, padding: 10, backgroundColor: '#f0f0f0', borderRadius: 5 }}>
-    //   <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{title}</Text>
-    //   <Text style={{ fontSize: 16 }}>Price: ${price}</Text>
-    //   <Text numberOfLines={2}>{description}</Text>
-    //   <Text>Rating: {rating}</Text>
-    //   <Text>Brand: {brand}</Text>
-    // </View>
-    <View style={{ flex: 1, padding: 10 }}>
-      <View style={{ backgroundColor: '#efe', flexDirection: 'row', padding: 10 }}>
-        <View style={{ flex: 1, alignItems: 'center'}}>
-          <Image
-            source={{ uri: imgUrl }}
-            style={{ width: 100, height: 100, borderRadius: 10 }}
-          />
-        </View>
-        <View style={{ flex: 3 }}>
-          <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{title}</Text>
-          <Text style={{ fontSize: 16 }}>Price: ${price}</Text>
-          <Text style={{fontSize: 16, color: 'green'}}>Discount: {discountPercentage}%</Text>
-          <Text>Description: {description}</Text>
-          <Text>Rating: {rating}</Text>
-          <Text>Brand: {brand}</Text>
-          <View style={{flexDirection: 'row'}}>
-              <Button>Detail</Button>
-              <Button>Add</Button>
-              <Button>Delete</Button>
-          </View>
-        </View>
-      </View>
-    </View>
-
-  );
-  return (
-
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.bigText}>Product list</Text>
-        <FlatList
-          data={data}
-          renderItem={({ item }) => <Item title={item.title} imgUrl={item.thumbnail} description={item.description} price={item.price} rating={item.rating} brand={item.brand} discountPercentage={item.discountPercentage} />}
-          keyExtractor={item => item.id}
-        />
-      </SafeAreaView>
-    </SafeAreaProvider>
-
+  return(
+      <ProductList />
   );
 }
 
-const styles = StyleSheet.create({
-  bigText: {
-    fontSize: 30,
-    color: '#aaa',
-    fontWeight: 'bold',
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
 
-});
 
 export default App;
---
+++ ProductsApp/ProductList.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import { FlatList, Image, ScrollView, StyleSheet } from 'react-native';
+import {
+  Text,
+  View,
+} from 'react-native';
+import { Button } from 'react-native-paper';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+const productList = () => {
+    const [data, setData] = useState([]);
+    const filePath = 'https://dummyjson.com/products';
+    useEffect(() => {
+        fetch(filePath)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((d) => {
+                setData(d.products);
+            })
+            .catch((error) => {
+                console.error('Error fetching data: ', error);
+            });
+    });
+    const Item = ({ imgUrl, title, description, price, rating, brand, discountPercentage }) => (
+        <View style={{ flex: 1, padding: 10 }}>
+            <View style={{ backgroundColor: '#efe', flexDirection: 'row', padding: 10 }}>
+                <View style={{ flex: 1, alignItems: 'center' }}>
+                    <Image
+                        source={{ uri: imgUrl }}
+                        style={{ width: 100, height: 100, borderRadius: 10 }}
+                    />
+                </View>
+                <View style={{ flex: 3 }}>
+                    <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{title}</Text>
+                    <Text style={{ fontSize: 14 }}>Price: ${price}</Text>
+                    <Text style={{ fontSize: 14, color: 'green' }}>Discount: {discountPercentage}% off</Text>
+                    <Text>Description: {description}</Text>
+                    <Text>Rating: {rating}</Text>
+                    <Text>Brand: {brand}</Text>
+                    <View style={{ flexDirection: 'row' }}>
+                        <Button>Detail</Button>
+                        <Button>Add</Button>
+                        <Button>Delete</Button>
+                    </View>
+                </View>
+            </View>
+        </View>
+
+    );
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container}>
+                <Text style={styles.bigText}>Product list</Text>
+                <FlatList
+                    data={data}
+                    renderItem={({ item }) => <Item title={item.title} imgUrl={item.thumbnail} description={item.description} price={item.price} rating={item.rating} brand={item.brand} discountPercentage={item.discountPercentage} />}
+                    keyExtractor={item => item.id}
+                />
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
+}
+const styles = StyleSheet.create({
+    bigText: {
+      fontSize: 30,
+      color: '#aaa',
+      fontWeight: 'bold',
+    },
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+  
+  });
+export default productList;
