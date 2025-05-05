b474b40547a0e96e743f8dab540e344fca42e5ad
Huy317 <167590855+Huy317@users.noreply.github.com>
2025-05-05 16:00:14 +0700

    removed unnecessary type

--- ProductsApp/App.tsx
+++ ProductsApp/App.tsx
@@ -7,16 +7,6 @@ import {
 import { Button } from 'react-native-paper';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import ProductList from './ProductList';
-type itemProps = {
-  imgUrl: string,
-  title: string,
-  description: string,
-  price: string,
-  rating: string,
-  brand: string,
-  discountPercentage: string
-};
-
 
 
 function App(): React.JSX.Element {
